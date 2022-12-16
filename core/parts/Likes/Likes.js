b922d69a42aa7797ea872cc561e6cb2c805d3948
7Kamil <115102676+7Kamil@users.noreply.github.com>
2022-12-16 11:05:08 +0400

    Update Likes.js

--- core/parts/Likes/Likes.js
+++ core/parts/Likes/Likes.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import './Favorites.css';
+import './Likes.css';
 
 
-class Favorites extends Component {
+class Likes extends Component {
     state = {
         title: 'Новый список',
-        movies: [
+        films: [
             { imdbID: 'tt0068646', title: 'The Godfather', year: 1972 }
         ]
     }
     render() { 
         return (
-            <div className="favorites">
-                <input value="Новый список" className="favorites__name" />
-                <ul className="favorites__list">
-                    {this.state.movies.map((item) => {
+            <div className="likes">
+                <input value="Новый список" className="likes__name" />
+                <ul className="likes__list">
+                    {this.state.likes.map((item) => {
                         return <li key={item.id}>{item.title} ({item.year})</li>;
                     })}
                 </ul>
-                <button type="button" className="favorites__save">Сохранить список</button>
+                <button type="button" className="likes__save">Сохранить список</button>
             </div>
         );
     }
