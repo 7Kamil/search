b4cbdc795ca43bd0657b2ae1eee8fb078b27be31
7Kamil <115102676+7Kamil@users.noreply.github.com>
2022-12-16 10:56:07 +0400

    Update FilmItem.js

--- core/parts/FilmItem/FilmItem.js
+++ core/parts/FilmItem/FilmItem.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import './MovieItem.css';
+import './FilmItem.css';
 
-class MovieItem extends Component {
+class FilmItem extends Component {
     render() {
         const { title, year, poster } = this.props;
         return (
-            <article className="movie-item">
-                <img className="movie-item__poster" src={poster} alt={title} />
-                <div className="movie-item__info">
-                    <h3 className="movie-item__title">{title}&nbsp;({year})</h3>
-                    <button type="button" className="movie-item__add-button">Добавить в список</button>
+            <article className="film-item">
+                <img className="film-item__poster" src={poster} alt={title} />
+                <div className="film-item__info">
+                    <h3 className="film-item__title">{title}&nbsp;({year})</h3>
+                    <button type="button" className="film-item__add-button">Добавить в список</button>
                 </div>
             </article>
         );
     }
 }
  
-export default MovieItem;
+export default FilmItem;
